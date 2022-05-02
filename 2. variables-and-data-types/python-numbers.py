192738094ade52176dacac333e7cb2e3d76a73bb
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-01 21:44:03 -0300

    feat: add python-numbers script

---
+++ 2. variables-and-data-types/python-numbers.py
@@ -0,0 +1,8 @@
+n_int = 10
+print(f"{n_int} is of type {type(n_int)}")
+
+n_float = 20.0
+print(f"{n_float} is of type {type(n_float)}")
+
+n_complex = 2+5j
+print(f"{n_complex} is complex number? {isinstance(1+2j,complex)}")
