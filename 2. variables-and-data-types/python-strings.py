0f4cdf75e18d9702f3da1dd450d68d97aff0b825
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-01 21:44:20 -0300

    feat: add python-strings script

---
+++ 2. variables-and-data-types/python-strings.py
@@ -0,0 +1,11 @@
+string = "Hello World!"
+print(string)                         # Prints complete string
+print(string[0])                      # Prints first character of the string
+print(string[2:5])                    # Prints characters starting from 3rd to 5th
+print(string[2:])                     # Prints string starting from 3rd character
+print(string * 2)                     # Prints string two times
+print(string + "We love Python ❤")   # Prints concatenated string
+string_multiple_lines = '''
+Hello World!
+'''
+print(string_multiple_lines)          # Prints complete string multiple lines
