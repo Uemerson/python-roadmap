3101b9fb984bb99295e98a307e1cfe97ce502c92
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-01 21:44:52 -0300

    feat: add python-tuple script

---
+++ 2. variables-and-data-types/python-tuple.py
@@ -0,0 +1,9 @@
+tuple = ('abcd', 900, 3.55, 'Uemerson', 80.1)
+tinytuple = (889, 'Uemerson')
+
+print(tuple)              # Prints the complete tuple
+print(tuple[0])           # Prints first element of the tuple
+print(tuple[1:3])         # Prints elements of the tuple starting from 2nd till 3rd
+print(tuple[2:])          # Prints elements of the tuple starting from 3rd element
+print(tinytuple * 2)      # Prints the contents of the tuple twice
+print(tuple + tinytuple)  # Prints concatenated tuples
