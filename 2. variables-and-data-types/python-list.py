4ed3f19d512972baf1014adad9cc50bcc32fb1d9
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-01 21:44:41 -0300

    feat: add python-list script

---
+++ 2. variables-and-data-types/python-list.py
@@ -0,0 +1,9 @@
+list = ['abcd', 900, 3.55, 'Uemerson', 80.1]
+tinylist = [889, 'Uemerson']
+
+print(list)             # Prints complete list
+print(list[0])          # Prints first element of the list
+print(list[1:3])        # Prints elements starting from 2nd till 3rd
+print(list[2:])         # Prints elements starting from 3rd element
+print(tinylist * 2)     # Prints list two times
+print(list + tinylist)  # Prints concatenated lists
