c77d590cfd1d7368e600a4fe8fb2a21c5585995c
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-06-28 14:35:39 -0300

    feat: add script linux_interaction

---
+++ 1-learn-the-basics/4-typecasting-exceptions/scripts/linux_interaction.py
@@ -0,0 +1,10 @@
+def linux_interaction():
+    import sys
+    if "linux" not in sys.platform:
+        raise RuntimeError("Function can only run on Linux systems.")
+    print("Doing Linux things.")
+
+try:
+    linux_interaction()
+except:
+    print("Linux function wasn't executed.")
